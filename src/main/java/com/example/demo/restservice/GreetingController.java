813bb63e1a4b68690d93680c51516aafc063134e
danyoh <danyoh#gmail.com>
2025-02-02 05:34:52 +0900

    logback 추가

--- src/main/java/com/example/demo/restservice/GreetingController.java
+++ src/main/java/com/example/demo/restservice/GreetingController.java
@@ -5,14 +5,17 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		log.info("Starting the application");
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
         
